e1b5ab1a5c7d1b7b69433cb6500d0cf66c0c75b4
Teodora <jevtic.teodora@gmail.com>
2021-01-05 16:58:00 +0100

    Added remote connection

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,18 @@
 <?php 
+    /*Development Connection
+
+    //Localhost connection
     $host = '127.0.0.1:8111';
     $db = 'teapp_db';
     $user = 'root';
     $pass = '';
+    $charset ='utf8mb4';
+    */
+    //Remote database connection
+    $host = 'remotemysql.com';
+    $db = 'PDjfc3dGVf';
+    $user = 'PDjfc3dGVf';
+    $pass = 'hP5RHxmSyY';
     $charset ='utf8mb4';
 
     $dsn =  "mysql:host=$host;dbname=$db;charset=$charset";         //nacin na koji se konektuje - data source name
